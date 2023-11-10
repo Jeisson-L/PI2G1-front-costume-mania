484141c208db8b0f52d6e92407664ddc8080aa00
Jeisson Lopez <jeisonma86@hotmail.com>
2023-11-09 22:10:26 -0500

    Adición merge sprint 2

--- src/components/Favorites/index.tsx
+++ src/components/Favorites/index.tsx
@@ -12,6 +12,15 @@ function Favorites() {
     if (favs) setFavs(JSON.parse(favs));
   }, []);
 
+  const handleFavRemove = (idModel: number) => {
+    const favs = localStorage.getItem("favs");
+    if (!favs) return;
+    const existingFavs: ApiCostume[] = JSON.parse(favs);
+    const newFavs = existingFavs.filter((e) => e.idModel !== idModel);
+    localStorage.setItem("favs", JSON.stringify(newFavs));
+    setFavs(newFavs);
+  };
+
   return (
     <section className="w-full rounded p-2 border mb-8">
       <h3 className="border-b-2 border-purple-3 py-2 mb-4 text-2xl font-bold">
@@ -19,21 +28,28 @@ function Favorites() {
       </h3>
 
       {favs.length ? (
-        <ul className="flex flex-col gap-4">
+        <ul className="flex flex-col gap-4 px-4">
           {favs.map((e) => (
-            <li key={e.idModel}>
+            <li key={e.idModel} className="flex justify-between">
               <Link href={`/costumes/${e.idModel}`} className="flex gap-4">
-                <Image
-                  src={e.urlImage || logo}
-                  alt={e.name}
-                  width={75}
-                  height={75}
-                />
+                <div className="w-20 flex justify-center">
+                  <Image
+                    src={e.urlImage || logo}
+                    alt={e.name}
+                    width={75}
+                    height={75}
+                    style={{
+                      maxHeight: "75px",
+                      width: "auto",
+                    }}
+                  />
+                </div>
                 <div className="flex flex-col justify-center">
                   <p>{e.name}</p>
                   <p>$ {e.price.toFixed(2)}</p>
                 </div>
               </Link>
+              <button onClick={() => handleFavRemove(e.idModel)}>X</button>
             </li>
           ))}
         </ul>
